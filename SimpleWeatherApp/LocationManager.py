8d579de6df4972f01a7049b62d0a513578941445
LukaszAcer <ufabia03@gmail.com>
2024-03-08 17:36:18 +0100

    bugs fixed

--- SimpleWeatherApp/LocationManager.py
+++ SimpleWeatherApp/LocationManager.py
@@ -1,9 +1,14 @@
 from geopy.geocoders import Nominatim
 
-def get_location(city: str) -> tuple[str, str]:
+
+def get_location(city: str) -> tuple[str, str] | None:
     geolocator = Nominatim(user_agent='location_manager')
     location = geolocator.geocode(city)
+    if location is None:
+        return None
+
     return location.latitude, location.longitude
-    
+
+
 if __name__ == '__main__':
     print(get_location("New York"))
--- SimpleWeatherApp/app.py
+++ SimpleWeatherApp/app.py
@@ -3,6 +3,8 @@ from WeatherRequest import WeatherRequest
 from LocationManager import get_location
 import socket
 
+NO_ANSWER = 'no such a city'
+
 app = Flask(__name__)
 
 
@@ -17,13 +19,16 @@ def home():
 
 @app.route('/search/<city>', methods=['POST', 'GET'])
 def search(city):
-    location = get_location(city=city) 
+    location = get_location(city=city)
+    if location is None:
+        return redirect(url_for('home'))
     weather = WeatherRequest(coords=location).wrap_in_data_model()
     return render_template('search.html', data=weather, value=city)
 
+
 @app.errorhandler(404)
-def page_not_found(e):
-    return home()
+def page_not_found():
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
